e4d01d5660be7c502c540dc4af7fa50b00600c3a
Jeff Swenson <jrswenson@users.noreply.github.com>
2021-01-29 15:35:19 -0700

    Created mock API, hero and villain services, and simple list on App component

--- src/App.js
+++ src/App.js
@@ -1,23 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from "react";
+import { getHeroes } from "./services/heroService";
+import { getVillains } from "./services/villainService";
 
 function App() {
+  const [heroes, setHeroes] = useState([]);
+  const [villains, setVillains] = useState([]);
+
+  useEffect(() => {
+    if (heroes.length === 0) {
+      getHeroes().then((heroes) => setHeroes(heroes));
+    }
+
+    if (heroes.length === 0) {
+      getVillains().then((villains) => setVillains(villains));
+    }
+  });
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Heroes</h1>
+      <ul>
+        {heroes.map((hero) => (
+          <li key={hero.id}>{hero.name}</li>
+        ))}
+      </ul>
+
+      <h1>Villains</h1>
+      <ul>
+        {villains.map((villain) => (
+          <li key={villain.id}>{villain.name}</li>
+        ))}
+      </ul>
     </div>
   );
 }
---
+++ src/services/villainService.js
@@ -0,0 +1,23 @@
+import { handleError, handleResponse } from "./serviceHelper";
+
+const baseUrl = process.env.REACT_APP_API_URL + "/villains/";
+
+export function getVillains() {
+  return fetch(baseUrl).then(handleResponse).catch(handleError);
+}
+
+export function saveVillain(hero) {
+  return fetch(baseUrl + (hero.id || ""), {
+    method: hero.id ? "PUT" : "POST", // POST for create, PUT to update when id already exists.
+    headers: { "content-type": "application/json" },
+    body: JSON.stringify(hero),
+  })
+    .then(handleResponse)
+    .catch(handleError);
+}
+
+export function deleteVillain(heroId) {
+  return fetch(baseUrl + heroId, { method: "DELETE" })
+    .then(handleResponse)
+    .catch(handleError);
+}
